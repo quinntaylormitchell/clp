db141d71853babd0dd0072728a4d10146422bcca
Quinn <q.mitchell@mail.utoronto.ca>
2026-04-02 17:40:43 +0000

    Implement verification for Presto queries.

--- integration-tests/tests/clp_package_tests/clp_presto/test_clp_presto.py
+++ integration-tests/tests/clp_package_tests/clp_presto/test_clp_presto.py
@@ -11,7 +11,12 @@ from tests.clp_package_tests.clp_presto.utils.classes import (
 from tests.clp_package_tests.clp_presto.utils.mode import (
     CLP_PRESTO_MODE,
 )
-from tests.clp_package_tests.clp_presto.utils.query import query_clp_presto
+from tests.clp_package_tests.clp_presto.utils.query import (
+    query_clp_presto,
+    verify_describe_dataset_action_clp_presto,
+    verify_select_logs_action_clp_presto,
+    verify_show_tables_action_clp_presto,
+)
 from tests.clp_package_tests.clp_presto.utils.split_filters import (
     add_split_filter_for_dataset,
     clear_split_filter_file,
@@ -48,13 +53,14 @@ def test_clp_presto_startup(clp_package: ClpPackage, presto_cluster: PrestoClust
 
 
 @pytest.mark.compression
-def test_clp_presto_compression_json_multifile(
+@pytest.mark.presto_query
+def test_clp_presto_json_multifile(
     clp_package: ClpPackage,
     presto_cluster: PrestoCluster,
     json_multifile: IntegrationTestDataset,
 ) -> None:
     """Docstring."""
-    logger.info("Starting test: 'test_clp_presto_compression_json_multifile'")
+    logger.info("Starting test: 'test_clp_presto_json_multifile'")
 
     clear_package_archives_clp_json(clp_package)
     clear_split_filter_file(presto_cluster)
@@ -62,48 +68,31 @@ def test_clp_presto_compression_json_multifile(
     compress_action = compress_clp_package(clp_package, json_multifile)
     verified, failure_message = verify_compress_action(compress_action, clp_package, json_multifile)
     assert verified, failure_message
+
     add_split_filter_for_dataset(json_multifile, presto_cluster)
 
-    # TODO: query for the dataset in Presto CLI (SHOW TABLES;)
-    # ^^^ actually should that be part of verification?
-    query_clp_presto(
+    show_tables_action = query_clp_presto(
         presto_cluster=presto_cluster,
         query="SHOW TABLES;",
     )
-    query_clp_presto(
+    verified, failure_message = verify_show_tables_action_clp_presto(show_tables_action, [json_multifile])
+    assert verified, failure_message
+
+    describe_dataset_action = query_clp_presto(
         presto_cluster=presto_cluster,
         query=f"DESCRIBE {json_multifile.dataset_name};",
     )
-
-    clear_split_filter_file(presto_cluster)
-    clear_package_archives_clp_json(clp_package)
-
-    logger.info("Test complete: 'test_clp_presto_compression_json_multifile'")
-
-
-@pytest.mark.search
-def test_clp_presto_query_basic_json_multifile(
-    clp_package: ClpPackage,
-    presto_cluster: PrestoCluster,
-    json_multifile: IntegrationTestDataset,
-) -> None:
-    """Docstring."""
-    logger.info("Starting test: 'test_clp_presto_query_basic_json_multifile'")
-
-    clear_package_archives_clp_json(clp_package)
-    clear_split_filter_file(presto_cluster)
-
-    compress_action = compress_clp_package(clp_package, json_multifile)
-    verified, failure_message = verify_compress_action(compress_action, clp_package, json_multifile)
+    verified, failure_message = verify_describe_dataset_action_clp_presto(describe_dataset_action, json_multifile)
     assert verified, failure_message
-    add_split_filter_for_dataset(json_multifile, presto_cluster)
 
-    # TODO: query for the dataset in Presto CLI (SHOW TABLES;)
-    # ^^^ actually should that be part of verification?
-
-    # TODO: basic Presto query
+    select_logs_action = query_clp_presto(
+        presto_cluster=presto_cluster,
+        query=f"SELECT * FROM {json_multifile.dataset_name};",
+    )
+    verified, failure_message = verify_select_logs_action_clp_presto(select_logs_action, json_multifile)
+    assert verified, failure_message
 
     clear_split_filter_file(presto_cluster)
     clear_package_archives_clp_json(clp_package)
 
-    logger.info("Test complete: 'test_clp_presto_query_basic_json_multifile'")
+    logger.info("Test complete: 'test_clp_presto_json_multifile'")
--- integration-tests/tests/clp_package_tests/clp_presto/utils/query.py
+++ integration-tests/tests/clp_package_tests/clp_presto/utils/query.py
@@ -1,11 +1,19 @@
 """Functions and classes to facilitate clp-presto queries."""
 
+import logging
+import json
+from typing import Any
+
+import pytest
 from clp_py_utils.clp_config import (
     PRESTO_COORDINATOR_COMPONENT_NAME,
 )
 
-from tests.clp_package_tests.clp_presto.utils.classes import PrestoCluster
+from tests.clp_package_tests.clp_presto.utils.classes import (
+    PrestoCluster,
+)
 from tests.utils.classes import (
+    IntegrationTestDataset,
     IntegrationTestExternalAction,
 )
 from tests.utils.subprocess_utils import execute_external_action
@@ -14,11 +22,17 @@ from tests.utils.utils import (
 )
 
 
+logger = logging.getLogger(__name__)
+
+
+
 def query_clp_presto(
     presto_cluster: PrestoCluster,
     query: str,
 ) -> IntegrationTestExternalAction:
     """Docstring."""
+    logger.info(f"Running Presto query: '{query}'")
+
     docker_compose_file_path = presto_cluster.path_config.docker_compose_file_path
     cmd: list[str] = [
         get_binary_path("docker"),
@@ -42,3 +56,95 @@ def query_clp_presto(
     execute_external_action(action)
 
     return action
+
+
+def verify_show_tables_action_clp_presto(
+    show_tables_action: IntegrationTestExternalAction,
+    current_datasets: list[IntegrationTestDataset],
+) -> tuple[bool, str]:
+    """Verify that `SHOW TABLES;` output is accurate w.r.t. current datasets."""
+    logger.info("Verifying `SHOW TABLES;` Presto query.")
+
+    output_lines = show_tables_action.completed_proc.stdout.splitlines()
+    try:
+        actual: set[str] = {json.loads(line)["Table"] for line in output_lines if line.strip()}
+    except (json.JSONDecodeError, KeyError) as e:
+        return False, f"Failed to parse output of `SHOW TABLES;` as JSON: {e}"
+
+    expected: set[str] = {ds.dataset_name for ds in current_datasets}
+
+    if actual != expected:
+        return (
+            False,
+            f"Mismatch between set of tables from `SHOW TABLES;` query: '{actual}' and expected"
+            f" set: '{expected}'",
+        )
+
+    return True, ""
+
+
+def verify_describe_dataset_action_clp_presto(
+    describe_dataset_action: IntegrationTestExternalAction,
+    dataset: IntegrationTestDataset,
+) -> tuple[bool, str]:
+    """
+    Verify that `DESCRIBE <dataset_name>;` output is accurate w.r.t. "columns" field from dataset
+    metadata.
+    """
+    logger.info("Verifying `DESCRIBE <dataset_name>;` Presto query.")
+
+    output_lines = describe_dataset_action.completed_proc.stdout.splitlines()
+    try:
+        actual: list[dict[str, Any]] = [json.loads(line) for line in output_lines if line.strip()]
+    except (json.JSONDecodeError, KeyError) as e:
+        return False, f"Failed to parse output of `DESCRIBE <dataset_name>;` as JSON: {e}"
+
+    expected: list[dict[str, Any]] = dataset.metadata_dict["columns"]
+
+    if actual != expected:
+        return (
+            False,
+            f"Mismatch between dataset column description from `DESCRIBE <dataset_name>;` query:"
+            f" '{actual}' and expected column description: '{expected}'",
+        )
+
+    return True, ""
+
+
+def verify_select_logs_action_clp_presto(
+    select_logs_action: IntegrationTestExternalAction,
+    dataset: IntegrationTestDataset,
+) -> tuple[bool, str]:
+    """
+    Verify that `SELECT * FROM <dataset_name>;` output is accurate w.r.t. grep -r ".*" output for
+    dataset logs.
+    """
+    logger.info("Verifying `SELECT * FROM <dataset_name>;` Presto query.")
+
+    actual = select_logs_action.completed_proc.stdout
+
+    cmd = [
+        get_binary_path("grep"),
+        "--recursive",
+        "--no-filename",
+        "--color=never",
+        ".*",
+        str(dataset.path_to_dataset_logs),
+    ]
+    grep_action = IntegrationTestExternalAction(cmd)
+    execute_external_action(grep_action)
+    if grep_action.completed_proc.returncode != 0:
+        pytest.fail(
+            "During `SELECT * FROM <dataset_name>;` verification, supporting `grep` call returned a"
+            f" non-zero exit code. Subprocess log: {grep_action.log_file_path}"
+        )
+    expected = grep_action.completed_proc.stdout
+
+    if actual != expected:
+        return (
+            False,
+            f"Mismatch between output logs from `SELECT * FROM <dataset_name>;` query: '{actual}'"
+            f" and expected logs: '{expected}'",
+        )
+
+    return True, ""
